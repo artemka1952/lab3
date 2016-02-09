f8640886fa0582f89bda15400039d7901a37d636
unknown <var1541b@k3-426-8.edu.ugrasu>
2016-02-09 10:03:23 +0500

    Сделал 1 задание

---
+++ JavaApplication1/src/task1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author var1541b
+ */
+public class task1 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        int j = 0;
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                a[j] = i;
+                System.out.println("a[" + j + "]=" + a[j]);
+                j++;
+            }
+        }
+
+    }
+}
